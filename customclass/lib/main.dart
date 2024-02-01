647b0b52f25abdf8fcd1199c7f90462699f05d6f
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-01 13:41:32 +0600

    customize app

--- customclass/lib/main.dart
+++ customclass/lib/main.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(Mycustomizeapp());
+}
+
+class Mycustomizeapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:"myfirst app",
+      theme: ThemeData(
+        primaryColor: Colors.blue
+      ),
+     home: Mydassboard()
+    );
+      //first Mycustomizeapp  run then Mydassboard home page hisabe rakci
+  }
+}
+
+class Mydassboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+
+      title: Text("Dashboard"),
+    
+     ),
+    body: Container(
+
+      color: Colors.black,
+    ),
+
+   );
+  }
+  
 }
